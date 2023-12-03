320a01ec126dcd36682a1050a570384ffbcdafd3
Henrik Gustafsson <gsson@fnord.se>
2023-12-03 21:33:04 +0000

    day03 refactoring and cleanup

--- problems/src/bin/day03/main.rs
+++ problems/src/bin/day03/main.rs
@@ -1,3 +1,4 @@
+use adventofcode2023_common::StrExt;
 use std::collections::{HashMap, HashSet};
 
 const INPUT: &str = include_str!("input.txt");
@@ -8,110 +9,86 @@ fn main() {
 }
 
 fn part1(input: &str) -> i32 {
-    let parts = input
-        .lines()
-        .enumerate()
-        .flat_map(|(y, line)| find_parts(y, line))
-        .collect::<HashSet<_>>();
+    let parts = parts_iter(input, next_part).collect::<HashSet<_>>();
 
-    let numbers = input
-        .lines()
-        .enumerate()
-        .flat_map(|(y, line)| find_numbers(y, line))
-        .collect::<Vec<_>>();
-
-    numbers
-        .into_iter()
+    numbers_iter(input)
         .filter(|((x1, x2), y, _)| adjacent(*x1, *x2, *y).any(|xy| parts.contains(&xy)))
-        .map(|(.., n)| n)
+        .map(|(.., n)| n.parse::<i32>().unwrap())
         .sum::<i32>()
 }
 
 fn part2(input: &str) -> i32 {
-    let gears = input
-        .lines()
-        .enumerate()
-        .flat_map(|(y, line)| find_gears(y, line))
-        .collect::<HashSet<_>>();
+    let gears = parts_iter(input, next_gear).collect::<HashSet<_>>();
 
-    let numbers = input
-        .lines()
-        .enumerate()
-        .flat_map(|(y, line)| find_numbers(y, line))
-        .collect::<Vec<_>>();
-
-    let ratios = numbers
-        .into_iter()
+    let ratios = numbers_iter(input)
         .flat_map(|((x1, x2), y, n)| adjacent(x1, x2, y).map(move |xy| (xy, n)))
         .filter(|(xy, ..)| gears.contains(xy))
-        .fold(
-            HashMap::<(i32, i32), Vec<i32>>::new(),
-            |mut ratios, (xy, n)| {
-                ratios.entry(xy).or_default().push(n);
-                ratios
-            },
-        );
+        .fold(HashMap::<_, Vec<_>>::new(), |mut ratios, (xy, n)| {
+            ratios.entry(xy).or_default().push(n);
+            ratios
+        });
 
     ratios
         .values()
-        .filter(|v| v.len() == 2)
-        .map(|v| v.iter().product::<i32>())
+        .filter_map(|v| match v[..] {
+            [a, b] => Some(a.to_i32() * b.to_i32()),
+            _ => None,
+        })
         .sum()
 }
 
+fn parts_iter(
+    input: &str,
+    searcher: fn(&str) -> Option<(usize, usize)>,
+) -> impl Iterator<Item = (i32, i32)> + '_ {
+    input
+        .lines()
+        .enumerate()
+        .flat_map(move |(y, line)| find(line, searcher).map(move |((x, _), _)| (x, y as i32)))
+}
+
+fn numbers_iter(input: &str) -> impl Iterator<Item = ((i32, i32), i32, &str)> + '_ {
+    input
+        .lines()
+        .enumerate()
+        .flat_map(|(y, line)| find(line, next_number).map(move |(xs, s)| (xs, y as i32, s)))
+}
+
 fn adjacent(x1: i32, x2: i32, y: i32) -> impl Iterator<Item = (i32, i32)> {
-    let left = std::iter::once((x1 - 1, y));
-    let right = std::iter::once((x2, y));
     let above = (x1 - 1..x2 + 1).map(move |x| (x, y - 1));
     let below = (x1 - 1..x2 + 1).map(move |x| (x, y + 1));
-    above.chain(below).chain(left).chain(right)
+    above.chain(below).chain([(x1 - 1, y), (x2, y)])
 }
 
-fn find_numbers(y: usize, line: &str) -> impl Iterator<Item = ((i32, i32), i32, i32)> + '_ {
-    fn next_number(p: usize, line: &str) -> Option<(usize, usize)> {
-        let start = line[p..].find(|c: char| c.is_ascii_digit())? + p;
-        let end = line[start..]
-            .find(|c: char| !c.is_ascii_digit())
-            .map(|e| e + start)
-            .unwrap_or(line.len());
-        Some((start, end))
-    }
+fn find(
+    line: &str,
+    searcher: fn(&str) -> Option<(usize, usize)>,
+) -> impl Iterator<Item = ((i32, i32), &str)> + '_ {
     let mut pos = 0;
     std::iter::from_fn(move || {
-        let (start, end) = next_number(pos, line)?;
+        let (start, end) = searcher(&line[pos..]).map(|(s, e)| (s + pos, e + pos))?;
         pos = end;
-        Some((
-            (start as i32, end as i32),
-            y as i32,
-            line[start..end].parse().unwrap(),
-        ))
+        Some(((start as i32, end as i32), &line[start..end]))
     })
 }
 
-fn find_parts(y: usize, line: &str) -> impl Iterator<Item = (i32, i32)> + '_ {
-    fn next(p: usize, line: &str) -> Option<usize> {
-        line[p..]
-            .find(|c: char| c != '.' && !c.is_ascii_digit())
-            .map(|s| s + p)
-    }
-    let mut pos = 0;
-    std::iter::from_fn(move || {
-        let x = next(pos, line)?;
-        pos = x + 1;
-        Some((x as i32, y as i32))
-    })
+fn next_number(line: &str) -> Option<(usize, usize)> {
+    let start = line.find(|c: char| c.is_ascii_digit())?;
+    let end = line[start..]
+        .find(|c: char| !c.is_ascii_digit())
+        .map(|pos| pos + start)
+        .unwrap_or(line.len());
+    Some((start, end))
 }
 
-fn find_gears(y: usize, line: &str) -> impl Iterator<Item = (i32, i32)> + '_ {
-    fn next(p: usize, line: &str) -> Option<usize> {
-        line[p..].find(|c: char| c == '*').map(|s| s + p)
-    }
-    let mut pos = 0;
-    std::iter::from_fn(move || {
-        let x = next(pos, line)?;
-        pos = x + 1;
-        Some((x as i32, y as i32))
-    })
+fn next_part(line: &str) -> Option<(usize, usize)> {
+    let start = line.find(|c: char| c != '.' && !c.is_ascii_digit())?;
+    Some((start, start + 1))
+}
+
+fn next_gear(line: &str) -> Option<(usize, usize)> {
+    let start = line.find(|c: char| c == '*')?;
+    Some((start, start + 1))
 }
 
 #[test]
