d9001ce9fc599231fdd49d3c76a39290291bd8a7
Henrik Gustafsson <gsson@fnord.se>
2023-12-03 21:32:36 +0000

    Add `StrExt` extension trait with panicking `to_i32()` method

--- common/src/lib.rs
+++ common/src/lib.rs
@@ -1,1 +1,14 @@
+use std::ops::Deref;
 
+pub trait StrExt {
+    fn to_i32(&self) -> i32;
+}
+
+impl<T> StrExt for T
+where
+    T: Deref<Target = str>,
+{
+    fn to_i32(&self) -> i32 {
+        self.parse::<i32>().unwrap()
+    }
+}
